41e9bb4e90bd4b747269d0be082227986cdac163
Arnaud Vinsard <avinsard@gmail.com>
2017-11-10 00:00:33 +0100

    Add IE detection script Add page error IE compatibility

--- js/main.js
+++ js/main.js
@@ -438,6 +438,7 @@
 	let pm, gfx;
 	DOM.body = document.body;
 	DOM.loading = document.querySelector('.loading');
+	DOM.errorIE = document.querySelector('.errorIE');
 	DOM.switchCtrls = document.querySelector('.switch');
 	DOM.switchModeCtrls = {
 		'design' : DOM.switchCtrls.firstElementChild,
@@ -476,9 +477,14 @@
 	let mode = 'design', disablePageFx, isAnimating;
 
 	function init() {
+		if(detectIE() == false){
+
 		imagesLoaded(DOM.body, { background: true }, function() {
 			// Remove page loader.
 			DOM.loading.classList.add('loading--hide');
+			//Remove page error IE.		
+			DOM.errorIE.classList.add('loading--hide');
+
 			// Create the image pieces.
 			pm = new PieceMaker(DOM.pieces);
 			// Start the squares loop effect on the main image.
@@ -490,6 +496,11 @@
 			// Init/Bind events
 			initEvents();
 		});
+	}else{
+		DOM.loading.classList.add('loading--hide');
+		//IE => show message error
+	}
+
 	}
 
 	function wordsToLetters() {
@@ -856,7 +867,45 @@
 		anime.remove(animeOpts.targets);
 		anime(animeOpts);
 	}
+	function detectIE(){
+		 var ua = window.navigator.userAgent;
 
+		  // Test values; Uncomment to check result …
+
+		  // IE 10
+		  // ua = 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)';
+		  
+		  // IE 11
+		  // ua = 'Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko';
+		  
+		  // Edge 12 (Spartan)
+		  // ua = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0';
+		  
+		  // Edge 13
+		  // ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586';
+
+		  var msie = ua.indexOf('MSIE ');
+		  if (msie > 0) {
+		    // IE 10 or older => return version number
+		    return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+		  }
+
+		  var trident = ua.indexOf('Trident/');
+		  if (trident > 0) {
+		    // IE 11 => return version number
+		    var rv = ua.indexOf('rv:');
+		    return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+		  }
+
+		  var edge = ua.indexOf('Edge/');
+		  if (edge > 0) {
+		    // Edge (IE 12+) => return version number
+		    return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+		  }
+
+		  // other browser
+		  return false;
+	}
 	init();
 
 })(window);
